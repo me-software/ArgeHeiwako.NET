804810b90fa9b28162dc56f56081ed861591117d
m4cx <macgyverx83@gmail.com>
2016-03-23 21:32:45 +0100

    Typ Satzfolgenummer hinzugefügt Issue #20

---
+++ src/ArgeHeiwako.Data/Common/Satzfolgenummer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ArgeHeiwako.Data.Common
+{
+    /// <summary>
+    /// Satzfolgenummer innerhalb eines Datenaustausch-Satzes
+    /// </summary>
+    public sealed class Satzfolgenummer
+    {
+        private readonly int satzfolgenummer;
+
+        /// <summary>
+        /// Erstellt eine neue <see cref="Satzfolgenummer"/>-Instanz
+        /// </summary>
+        /// <param name="satzfolgenummer">Der numerische Wert der Satzfolgenummer.</param>
+        public Satzfolgenummer(int satzfolgenummer)
+        {
+            if (satzfolgenummer < -999999 || satzfolgenummer > 9999999)
+                throw new ArgumentOutOfRangeException("satzfolgenummer");
+
+            this.satzfolgenummer = satzfolgenummer;
+        }
+
+        /// <summary>
+        /// Erstellt eine neue <see cref="Satzfolgenummer"/>-Instanz
+        /// </summary>
+        /// <param name="satzfolgenummer">Der textuelle Wert der Satzfolgenummer aus Datenaustauschdatei.</param>
+        public Satzfolgenummer(string satzfolgenummer)
+        {
+            if (satzfolgenummer == null)
+                throw new ArgumentNullException("satzfolgenummer");
+
+            if (satzfolgenummer.Length != 7)
+                throw new ArgumentOutOfRangeException("satzfolgenummer");
+
+            int value = 0;
+            if (!int.TryParse(satzfolgenummer, out value))
+                throw new ArgumentOutOfRangeException("satzfolgenummer");
+
+            this.satzfolgenummer = value;
+        }
+
+        /// <summary>
+        /// Implizite Konvertierung einer <see cref="Satzfolgenummer"/>-Instanz in den numerischen Wert vom Typ <see cref="int"/>
+        /// </summary>
+        /// <param name="folgenummer">Die zu konvertierende <see cref="Satzfolgenummer"/>-Instanz</param>
+        /// <returns>Der numerische Wert der Satzfolgenummer</returns>
+        public static implicit operator int(Satzfolgenummer folgenummer)
+        {
+            return folgenummer.satzfolgenummer;
+        }
+    }
+}
---
+++ src/ArgeHeiwako.Tests/Data/Common/SatzfolgenummerTests.cs
@@ -0,0 +1,69 @@
+﻿using ArgeHeiwako.Data.Common;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace ArgeHeiwako.Tests.Data.Common
+{
+    [ExcludeFromCodeCoverage]
+    public class SatzfolgenummerTests
+    {
+        #region Ctor
+
+        [Fact]
+        public void Ctor_Null_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new Satzfolgenummer(null));
+            Assert.Equal("satzfolgenummer", ex.ParamName);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("000000")]
+        [InlineData("00000000")]
+        [InlineData("AAAAAAA")]
+        public void Ctor_IncorrectString_ThrowsArgumentOutOfRangeException(string data)
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Satzfolgenummer(data));
+            Assert.Equal("satzfolgenummer", ex.ParamName);
+        }
+
+        [Theory]
+        [InlineData(10000000)]
+        [InlineData(-1000000)]
+        public void Ctor_IntOutOfRange_ThrowsArgumentOutOfRangeException(int data)
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Satzfolgenummer(data));
+            Assert.Equal("satzfolgenummer", ex.ParamName);
+        }
+
+        #endregion
+
+        #region Op_Implicit Int
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        [InlineData(10000)]
+        [InlineData(100000)]
+        [InlineData(1000000)]
+        [InlineData(-10)]
+        [InlineData(-100)]
+        [InlineData(-1000)]
+        public void OpImplicitInt_ReturnsValue(int data)
+        {
+            Assert.Equal(data, new Satzfolgenummer(data));
+        }
+
+        [Theory]
+        [InlineData("0000001", 1)]
+        [InlineData("-000001", -1)]
+        public void OpImplicitInt_StringValue_ReturnsIntValue(string data, int value)
+        {
+            Assert.Equal(value, new Satzfolgenummer(data));
+        }
+        #endregion
+    }
+}
