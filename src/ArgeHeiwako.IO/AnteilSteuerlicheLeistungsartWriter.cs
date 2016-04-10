d90281ba7d49db7516d6131be03f6aba428d8354
m4cx <macgyverx83@gmail.com>
2016-04-10 19:28:52 +0200

    AnteilSteuerlicheLeistungsartWriter überarbeitet - Zentrale Optionen in Basisklasse ausgelagert. Issue #20 Issue #22

--- src/ArgeHeiwako.IO/AnteilSteuerlicheLeistungsartWriter.cs
+++ src/ArgeHeiwako.IO/AnteilSteuerlicheLeistungsartWriter.cs
@@ -1,5 +1,4 @@
 ﻿using ArgeHeiwako.Data;
-using System;
 using System.IO;
 
 namespace ArgeHeiwako.IO
@@ -7,57 +6,15 @@ namespace ArgeHeiwako.IO
     /// <summary>
     /// Implementierung des <see cref="IArgeWriter{T}"/> der sich um das Schreiben von <see cref="AnteilSteuerlicheLeistungsart"/> kümmert
     /// </summary>
-    public class AnteilSteuerlicheLeistungsartWriter : IArgeWriter<AnteilSteuerlicheLeistungsart>
+    public class AnteilSteuerlicheLeistungsartWriter : ArgeWriter<AnteilSteuerlicheLeistungsart>
     {
-        private StreamWriter writer;
-
         /// <summary>
         /// Erstellt eine neue <see cref="AnteilSteuerlicheLeistungsartWriter"/>-Instanz
         /// </summary>
         /// <param name="stream"></param>
         public AnteilSteuerlicheLeistungsartWriter(Stream stream)
+            : base(stream)
         {
-            writer = new StreamWriter(stream);
         }
-
-        /// <summary>
-        /// Schreibt eine Datenaustauschsatz-Instanz in die verwendete Quelle
-        /// </summary>
-        /// <param name="datensatz"></param>
-        public void Write(AnteilSteuerlicheLeistungsart datensatz)
-        {
-            if (disposedValue)
-                throw new ObjectDisposedException(nameof(AnteilSteuerlicheLeistungsartWriter));
-
-            writer.Write($"{datensatz}\r\n");
-        }
-
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="disposing"></param>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    writer.Dispose();
-                }
-                disposedValue = true;
-            }
-        }
-
-        /// <summary>
-        /// Öffentliche Implemntierung des Dispose-Pattern für die externe Verwendung
-        /// </summary>
-        public void Dispose()
-        {
-            Dispose(true);
-        }
-        #endregion
     }
 }
---
+++ src/ArgeHeiwako.IO/ArgeWriter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ArgeHeiwako.IO
+{
+    /// <summary>
+    /// Basisimplementierung der <see cref="IArgeWriter{T}"/> Schnittstelle für das Schreiben von 
+    /// Datenaustauschinformationen
+    /// </summary>
+    /// <typeparam name="TInstance"></typeparam>
+    public abstract class ArgeWriter<TInstance> : IArgeWriter<TInstance>
+    {
+        private const int ISO_8859_15_CODE_PAGE = 28605;
+        private const string LINE_END = "\r\n";
+        private StreamWriter writer;
+
+        /// <summary>
+        /// Liefert das verwendete <see cref="Encoding"/>
+        /// </summary>
+        public static readonly Encoding WriterEncoding = Encoding.GetEncoding(ISO_8859_15_CODE_PAGE);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stream"></param>
+        protected ArgeWriter(Stream stream)
+        {
+            writer = new StreamWriter(stream, WriterEncoding);
+        }
+
+        /// <summary>
+        /// Schreibt eine Datenaustauschsatz-Instanz in die verwendete Quelle
+        /// </summary>
+        /// <param name="datensatz"></param>
+        public void Write(TInstance datensatz)
+        {
+            if (isDisposed)
+                throw new ObjectDisposedException(this.GetType().Name);
+
+            writer.Write($"{datensatz}{LINE_END}");
+        }
+
+        #region IDisposable Support
+        private bool isDisposed = false; // To detect redundant calls
+
+        /// <summary>
+        /// Private Implementierung des <see cref="IDisposable"/>-Pattern
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    writer.Dispose();
+                }
+                
+                isDisposed = true;
+            }
+        }
+        
+        /// <summary>
+        /// Öffentliche Implementierung des <see cref="IDisposable"/>-Pattern
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
