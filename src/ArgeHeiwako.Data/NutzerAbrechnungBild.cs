d4037c0e05786de093e624c9a71dee959fc6adb0
m4cx <macgyverx83@gmail.com>
2016-04-21 23:20:56 +0200

    NutzerAbrechnungBild begonnen - Erstellen und Serialisierung implementiert. Issue #19

---
+++ src/ArgeHeiwako.Data/NutzerAbrechnungBild.cs
@@ -0,0 +1,148 @@
+﻿using ArgeHeiwako.Data.Common;
+using System;
+using System.Collections.Generic;
+
+namespace ArgeHeiwako.Data
+{
+    /// <summary>
+    /// Diese Klasse repräsentiert einen E898-Datensatz, welcher zum Austausch von Abrechnungsdaten in Dokumentform (Bild, PDF, o.a.) genutzt wird.
+    /// </summary>
+    public class NutzerAbrechnungBild
+    {
+        private const string Satzart = "E898";
+
+        private readonly BilddateiFolgeNummer bilddateiFolgeNummer;
+        private readonly BilddateiPfad bilddateiPfad;
+        private readonly Tag letzterTagNutzungszeitraum;
+        private readonly ErweiterterOrdnungsbegriffAbrechnungsunternehmen ordnungsbegriffAbrechnungsunternehmen;
+        private readonly OrdnungsbegriffWohnungsunternehmen ordnungsbegriffWohnungsunternehmen;
+        private readonly SatzfolgeNummer satzfolgeNummer;
+        private readonly Abrechnungsunternehmen abrechnungsunternehmen;
+        private readonly AbrechnungsfolgeNummer abrechnungsfolgeNummer;
+        private readonly Dokumentart dokumentart;
+
+        /// <summary>
+        /// Erstellt eine neue <see cref="NutzerAbrechnungBild"/>-Instanz
+        /// </summary>
+        /// <param name="ordnungsbegriffAbrechnungsunternehmen"></param>
+        /// <param name="ordnungsbegriffWohnungsunternehmen"></param>
+        /// <param name="bilddateiPfad"></param>
+        /// <param name="bilddateiFolgeNummer"></param>
+        /// <param name="letzterTagNutzungszeitraum"></param>
+        /// <param name="satzfolgeNummer"></param>
+        /// <param name="abrechnungsunternehmen"></param>
+        /// <param name="abrechnungsfolgeNummer"></param>
+        /// <param name="dokumentart"></param>
+        public NutzerAbrechnungBild(
+            ErweiterterOrdnungsbegriffAbrechnungsunternehmen ordnungsbegriffAbrechnungsunternehmen,
+            OrdnungsbegriffWohnungsunternehmen ordnungsbegriffWohnungsunternehmen,
+            BilddateiPfad bilddateiPfad,
+            BilddateiFolgeNummer bilddateiFolgeNummer,
+            Tag letzterTagNutzungszeitraum,
+            SatzfolgeNummer satzfolgeNummer = null,
+            Abrechnungsunternehmen abrechnungsunternehmen = null,
+            AbrechnungsfolgeNummer abrechnungsfolgeNummer = null,
+            Dokumentart dokumentart = null)
+        {
+            if (ordnungsbegriffAbrechnungsunternehmen == null)
+                throw new ArgumentNullException(nameof(ordnungsbegriffAbrechnungsunternehmen));
+            if (ordnungsbegriffWohnungsunternehmen == null)
+                throw new ArgumentNullException(nameof(ordnungsbegriffWohnungsunternehmen));
+            if (bilddateiPfad == null)
+                throw new ArgumentNullException(nameof(bilddateiPfad));
+            if (bilddateiFolgeNummer == null)
+                throw new ArgumentNullException(nameof(bilddateiFolgeNummer));
+            if (letzterTagNutzungszeitraum == null)
+                throw new ArgumentNullException(nameof(letzterTagNutzungszeitraum));
+
+            this.ordnungsbegriffAbrechnungsunternehmen = ordnungsbegriffAbrechnungsunternehmen;
+            this.ordnungsbegriffWohnungsunternehmen = ordnungsbegriffWohnungsunternehmen;
+            this.bilddateiPfad = bilddateiPfad;
+            this.bilddateiFolgeNummer = bilddateiFolgeNummer;
+            this.letzterTagNutzungszeitraum = letzterTagNutzungszeitraum;
+            this.satzfolgeNummer = satzfolgeNummer;
+            this.abrechnungsunternehmen = abrechnungsunternehmen;
+            this.abrechnungsfolgeNummer = abrechnungsfolgeNummer;
+            this.dokumentart = dokumentart;
+        }
+
+        /// <summary>
+        /// Liefert den Schlüssel des Abrechnungsunternehmens
+        /// </summary>
+        public ErweiterterOrdnungsbegriffAbrechnungsunternehmen OrdnungsbegriffAbrechnungsunternehmen
+        {
+            get { return ordnungsbegriffAbrechnungsunternehmen; }
+        }
+
+        /// <summary>
+        /// Liefert den Ordnungsbegriff des Wohnungsunternehmens / Auftraggebers
+        /// </summary>
+        public OrdnungsbegriffWohnungsunternehmen OrdnungsbegriffWohnungsunternehmen
+        {
+            get { return ordnungsbegriffWohnungsunternehmen; }
+        }
+
+        /// <summary>
+        /// Liefert die Bilddatei (Pfad)
+        /// </summary>
+        public BilddateiPfad BilddateiPfad
+        {
+            get { return bilddateiPfad; }
+        }
+
+        /// <summary>
+        /// Liefert die Bilddateti-Folgenummer (Seitenzahl bei einer Seite pro Datei)
+        /// </summary>
+        public BilddateiFolgeNummer BilddateiFolgeNummer
+        {
+            get { return bilddateiFolgeNummer; }
+        }
+
+        /// <summary>
+        /// Liefert den letzten Tag des Nutzungszeitraumes
+        /// </summary>
+        public Tag LetzterTagNutzungszeitraum
+        {
+            get { return letzterTagNutzungszeitraum; }
+        }
+
+        /// <summary>
+        /// Liefert die optionale Satzfolgenummer
+        /// </summary>
+        public SatzfolgeNummer SatzfolgeNummer { get { return satzfolgeNummer; } }
+
+        /// <summary>
+        /// Liefert das optionale Abrechnungsunternehmen
+        /// </summary>
+        public Abrechnungsunternehmen Abrechnungsunternehmen { get { return abrechnungsunternehmen; } }
+
+        /// <summary>
+        /// Liefert das optionale Kennzeichen für die Abrechnungsfolgenummer
+        /// </summary>
+        public AbrechnungsfolgeNummer AbrechnungsfolgeNummer { get { return abrechnungsfolgeNummer; } }
+
+        /// <summary>
+        /// Liefert die optionale Dokumentart
+        /// </summary>
+        public Dokumentart Dokumentart { get { return dokumentart; } }
+
+        /// <summary>
+        /// Liefert die formatierte Ausgabe für die Verwendung in einer Datentausch-Datei
+        /// </summary>
+        /// <returns>Der formatierte String für die Datenaustauschdatei</returns>
+        public override string ToString()
+        {
+            return string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}",
+                Satzart,
+                (satzfolgeNummer == null) ? new string(' ', 7) : satzfolgeNummer.ToString(),
+                (abrechnungsunternehmen == null) ? new string(' ', 2) : abrechnungsunternehmen.ToString(),
+                ordnungsbegriffAbrechnungsunternehmen,
+                ordnungsbegriffWohnungsunternehmen,
+                (abrechnungsfolgeNummer == null) ? " " : abrechnungsfolgeNummer.ToString(),
+                bilddateiPfad,
+                bilddateiFolgeNummer,
+                letzterTagNutzungszeitraum,
+                (dokumentart == null) ? "   " : dokumentart.ToString());
+        }
+    }
+}
---
+++ src/ArgeHeiwako.Tests/Data/NutzerAbrechnungBildTests.cs
@@ -0,0 +1,213 @@
+﻿using ArgeHeiwako.Data;
+using ArgeHeiwako.Data.Common;
+using ArgeHeiwako.Tests.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArgeHeiwako.Tests.Data
+{
+    public class NutzerAbrechnungBildTests
+    {
+        private static LiegenschaftsNummer liegenschaftsNummer = new LiegenschaftsNummer(1);
+        private static NutzergruppenNummer nutzergruppenNummer = new NutzergruppenNummer(1);
+        private static WohnungsNummer wohnungsNummer = new WohnungsNummer(1);
+        private static Nutzerfolge nutzerfolge = new Nutzerfolge(1);
+        private static ErweiterterOrdnungsbegriffAbrechnungsunternehmen ordnungsbegriffAbrechnungsunternehmen = new ErweiterterOrdnungsbegriffAbrechnungsunternehmen(liegenschaftsNummer, nutzergruppenNummer, wohnungsNummer, nutzerfolge);
+        private static OrdnungsbegriffWohnungsunternehmen ordnungsbegriffWohnungsunternehmen = new OrdnungsbegriffWohnungsunternehmen("ID");
+        private static BilddateiPfad bilddateiPfad = new BilddateiPfad("test.pdf");
+        private static BilddateiFolgeNummer bilddateiFolgeNummer = new BilddateiFolgeNummer(1);
+        private static Tag letzterTagNutzungszeitraum = new Tag(new DateTime(2015, 12, 31));
+
+        #region Ctor
+
+        [Fact]
+        public void Ctor_OrdnungsbegriffAbrechnungsunternehmenNull_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new NutzerAbrechnungBild(null, ordnungsbegriffWohnungsunternehmen, bilddateiPfad, bilddateiFolgeNummer, letzterTagNutzungszeitraum));
+            Assert.Equal("ordnungsbegriffAbrechnungsunternehmen", ex.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_OrdnungsbegriffWohnungsunternehmenNull_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new NutzerAbrechnungBild(ordnungsbegriffAbrechnungsunternehmen, null, bilddateiPfad, bilddateiFolgeNummer, letzterTagNutzungszeitraum));
+            Assert.Equal("ordnungsbegriffWohnungsunternehmen", ex.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_BilddateiPfadNull_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new NutzerAbrechnungBild(ordnungsbegriffAbrechnungsunternehmen, ordnungsbegriffWohnungsunternehmen, null, bilddateiFolgeNummer, letzterTagNutzungszeitraum));
+            Assert.Equal("bilddateiPfad", ex.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_BilddateiFolgeNummerNull_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new NutzerAbrechnungBild(ordnungsbegriffAbrechnungsunternehmen, ordnungsbegriffWohnungsunternehmen, bilddateiPfad, null, letzterTagNutzungszeitraum));
+            Assert.Equal("bilddateiFolgeNummer", ex.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_LetzterTagNutzungszeitraumNull_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new NutzerAbrechnungBild(ordnungsbegriffAbrechnungsunternehmen, ordnungsbegriffWohnungsunternehmen, bilddateiPfad, bilddateiFolgeNummer, null));
+            Assert.Equal("letzterTagNutzungszeitraum", ex.ParamName);
+        }
+
+        #endregion
+
+        #region OrdnungsbegriffAbrechnungsunternehmen
+
+        [Fact]
+        public void OrdnungsbegriffAbrechnungsunternehmen_Get_ReturnsInstance()
+        {
+            Assert.Equal(ordnungsbegriffAbrechnungsunternehmen, CreateDefault().OrdnungsbegriffAbrechnungsunternehmen);
+        }
+
+        #endregion
+
+        #region OrdnungsbegriffWohnungsunternehmen
+
+        [Fact]
+        public void OrdnungsbegriffWohnungsunternehmen_Get_ReturnsInstance()
+        {
+            Assert.Equal(ordnungsbegriffWohnungsunternehmen, CreateDefault().OrdnungsbegriffWohnungsunternehmen);
+        }
+
+        #endregion
+
+        #region BilddateiPfad
+
+        [Fact]
+        public void BilddateiPfad_Get_ReturnsInstance()
+        {
+            Assert.Equal(bilddateiPfad, CreateDefault().BilddateiPfad);
+        }
+
+        #endregion
+
+        #region BilddateiFolgeNummer
+
+        [Fact]
+        public void BilddateiFolgeNummer_Get_ReturnsInstance()
+        {
+            Assert.Equal(bilddateiFolgeNummer, CreateDefault().BilddateiFolgeNummer);
+        }
+
+        #endregion
+
+        #region LetzterTagNutzungszeitraum
+
+        [Fact]
+        public void LetzterTagNutzungszeitraum_Get_ReturnsInstance()
+        {
+            Assert.Equal(letzterTagNutzungszeitraum, CreateDefault().LetzterTagNutzungszeitraum);
+        }
+
+        #endregion
+
+        #region SatzfolgeNummer
+
+        [Fact]
+        public void SatzfolgeNummer_Get_DefaultReturnsNull()
+        {
+            Assert.Null(CreateDefault().SatzfolgeNummer);
+        }
+
+        [Fact]
+        public void SatzfolgeNummer_Get_SetByCtorReturnsInstance()
+        {
+            var satzfolgeNummer = new SatzfolgeNummer(1);
+            var instance = new NutzerAbrechnungBild(ordnungsbegriffAbrechnungsunternehmen, ordnungsbegriffWohnungsunternehmen, bilddateiPfad, bilddateiFolgeNummer, letzterTagNutzungszeitraum, satzfolgeNummer: satzfolgeNummer);
+            Assert.Equal(satzfolgeNummer, instance.SatzfolgeNummer);
+        }
+
+        #endregion
+
+        #region Abrechnungsunternehmen
+
+        [Fact]
+        public void Abrechnungsunternehmen_Get_DefaultReturnsNull()
+        {
+            Assert.Null(CreateDefault().Abrechnungsunternehmen);
+        }
+
+        [Fact]
+        public void Abrechnungsunternehmen_Get_SetByCtorReturnsInstance()
+        {
+            var abrechnungsunternehmen = Abrechnungsunternehmen.Finde(30);
+            var instance = new NutzerAbrechnungBild(ordnungsbegriffAbrechnungsunternehmen, ordnungsbegriffWohnungsunternehmen, bilddateiPfad, bilddateiFolgeNummer, letzterTagNutzungszeitraum, abrechnungsunternehmen: abrechnungsunternehmen);
+            Assert.Equal(abrechnungsunternehmen, instance.Abrechnungsunternehmen);
+        }
+
+        #endregion
+
+        #region AbrechnungsfolgeNummer
+
+        [Fact]
+        public void AbrechnungsfolgeNummer_Get_DefaultReturnsNull()
+        {
+            Assert.Null(CreateDefault().AbrechnungsfolgeNummer);
+        }
+
+        [Fact]
+        public void AbrechnungsfolgeNummer_Get_SetByCtorReturnsInstance()
+        {
+            var abrechnungsfolgeNummer = new AbrechnungsfolgeNummer("1");
+            var instance = new NutzerAbrechnungBild(ordnungsbegriffAbrechnungsunternehmen, ordnungsbegriffWohnungsunternehmen, bilddateiPfad, bilddateiFolgeNummer, letzterTagNutzungszeitraum, abrechnungsfolgeNummer: abrechnungsfolgeNummer);
+            Assert.Equal(abrechnungsfolgeNummer, instance.AbrechnungsfolgeNummer);
+        }
+
+        #endregion
+
+        #region Dokumentart
+
+        [Fact]
+        public void Dokumentart_Get_DefaultReturnsNull()
+        {
+            Assert.Null(CreateDefault().Dokumentart);
+        }
+
+        [Fact]
+        public void Dokumentart_Get_SetByCtorReturnsInstance()
+        {
+            var dokumentart = Dokumentart.Heizkostenabrechnung;
+            var instance = new NutzerAbrechnungBild(ordnungsbegriffAbrechnungsunternehmen, ordnungsbegriffWohnungsunternehmen, bilddateiPfad, bilddateiFolgeNummer, letzterTagNutzungszeitraum, dokumentart: dokumentart);
+            Assert.Equal(dokumentart, instance.Dokumentart);
+        }
+
+        #endregion
+
+        #region ToString()
+
+        [Fact]
+        public void ToString_DefaultInstance_ReturnsStringLength120()
+        {
+            Assert.Equal(120, CreateDefault().ToString().Length);
+        }
+
+        [Fact]
+        public void ToString_DefaultInstance_ReturnsStringValue()
+        {
+            var expectedString = "E898         000000001000100011ID                   test.pdf                                                001311215   ";
+            Assert.Equal(expectedString, CreateDefault().ToString());
+        }
+
+        #endregion
+
+        internal static NutzerAbrechnungBild CreateDefault()
+        {
+            return new NutzerAbrechnungBild(
+                ordnungsbegriffAbrechnungsunternehmen,
+                ordnungsbegriffWohnungsunternehmen,
+                bilddateiPfad,
+                bilddateiFolgeNummer,
+                letzterTagNutzungszeitraum);
+        }
+    }
+}
