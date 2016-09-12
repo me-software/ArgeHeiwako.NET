48a0a2a09d0ad7eaf83ccabacd758dc96444705a
m4cx <macgyverx83@gmail.com>
2016-09-12 21:40:20 +0200

    #30 Einheit (Tabelle "E") aus dem Datenaustausch bereitgestellt.

---
+++ src/ArgeHeiwako.Data/Common/Einheit.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArgeHeiwako.Data.Common
+{
+    /// <summary>
+    /// Diese Klasse repräsentiert die im Datenaustausch definierten Einheiten für entsprechende Angaben innerhalb 
+    /// der Datenaustauschdatei gemäß der Tabelle "E"
+    /// </summary>
+    public sealed class Einheit
+    {
+        #region Tabelle "E"
+
+        private static readonly IDictionary<string, Einheit> einheiten = new Dictionary<string, Einheit>
+        {
+            { "001", GJ = new Einheit("001","GJ") },
+            { "002", MWh = new Einheit("002","MWh") },
+            { "003", Kubikmeter = new Einheit("003","m³") },
+            { "004", Kalorien = new Einheit("004","kcal") },
+            { "005", KilowattStunde = new Einheit("005","kWh") },
+            { "010", QmWohnflaeche = new Einheit("010","m² Wohnfläche") },
+            { "011", QmBeheizteWohnflaeche = new Einheit("011","m² beheizte Wohnfläche") },
+            { "012", CmUmbauterRaum = new Einheit("012","m³ umbauter Raum") },
+            { "014", CmBeheizterUmbauterRaum = new Einheit("014","m³ beheizter umbauter Wohnraum") },
+            { "015", VariablesTextfeld = new Einheit("015","variables Textfeld für Schlüssel") },
+            { "016", Miteigentumsanteil = new Einheit("016","Miteigentumsanteil") },
+            { "017", QmNutzflaeche = new Einheit("017","m² Nutzfläche") },
+            { "020", Anschlusswert = new Einheit("020","Anschlusswert") },
+            { "021", Zaehler = new Einheit("021","Zähler") },
+            { "022", Wohnung = new Einheit("022","Wohnung") },
+            { "023", Abrechnung = new Einheit("023","Abrechnung") },
+            { "030", Verbrauchseinheiten = new Einheit("030","Verbrauchseinheiten (VE)") },
+            { "031", Verbrauchswerte = new Einheit("031","Verbrauchswerte") },
+            { "032", Striche = new Einheit("032","Striche (Venturi)") },
+            { "033", KilojouleProSekunde = new Einheit("033","1000 J/Sec.") },
+            { "034", PersonenMalMonate = new Einheit("034","Personen x Monate") },
+            { "035", Personen = new Einheit("035","Personen") },
+            { "040", Prozent = new Einheit("040","Prozent") },
+            { "041", Jahre = new Einheit("041","Jahr") },
+            { "042", Monate = new Einheit("042","Monat") },
+            { "043", Tage = new Einheit("043","Tage") },
+            { "044", Gradtage = new Einheit("044","Gradtage") },
+            { "045", PromilleAnteile = new Einheit("045","‰ – Anteile") },
+            { "046", AnzahlRauchwarnmelder = new Einheit("046","Anzahl Rauchwarnmelder") }
+        };
+
+        #endregion
+
+        #region Schnellzugriff auf Einheiten
+
+        /// <summary>
+        /// GJ
+        /// </summary>
+        public static Einheit GJ { get; private set; }
+
+        /// <summary>
+        /// MWh
+        /// </summary>
+        public static Einheit MWh { get; private set; }
+
+        /// <summary>
+        /// m³
+        /// </summary>
+        public static Einheit Kubikmeter { get; private set; }
+
+        /// <summary>
+        /// kcal
+        /// </summary>
+        public static Einheit Kalorien { get; private set; }
+
+        /// <summary>
+        /// kWh
+        /// </summary>
+        public static Einheit KilowattStunde { get; private set; }
+
+        /// <summary>
+        /// m² Wohnfläche
+        /// </summary>
+        public static Einheit QmWohnflaeche { get; private set; }
+
+        /// <summary>
+        /// m² beheizte Wohnfläche
+        /// </summary>
+        public static Einheit QmBeheizteWohnflaeche { get; private set; }
+
+        /// <summary>
+        /// m³ umbauter Raum
+        /// </summary>
+        public static Einheit CmUmbauterRaum { get; private set; }
+
+        /// <summary>
+        /// m³ beiheiter umbauter Raum
+        /// </summary>
+        public static Einheit CmBeheizterUmbauterRaum { get; private set; }
+
+        /// <summary>
+        /// Variables Textfeld für Einheit
+        /// </summary>
+        public static Einheit VariablesTextfeld { get; private set; }
+
+        /// <summary>
+        /// Miteigentumsanteil
+        /// </summary>
+        public static Einheit Miteigentumsanteil { get; private set; }
+
+        /// <summary>
+        /// m² Nutzfläche
+        /// </summary>
+        public static Einheit QmNutzflaeche { get; private set; }
+
+        /// <summary>
+        /// Anschlusswert
+        /// </summary>
+        public static Einheit Anschlusswert { get; private set; }
+
+        /// <summary>
+        /// Zähler
+        /// </summary>
+        public static Einheit Zaehler { get; private set; }
+
+        /// <summary>
+        /// Wohnung
+        /// </summary>
+        public static Einheit Wohnung { get; private set; }
+
+        /// <summary>
+        /// Abrechnung
+        /// </summary>
+        public static Einheit Abrechnung { get; private set; }
+
+        /// <summary>
+        /// Verbrauchseinheiten
+        /// </summary>
+        public static Einheit Verbrauchseinheiten { get; private set; }
+
+        /// <summary>
+        /// Verbrauchswerte
+        /// </summary>
+        public static Einheit Verbrauchswerte { get; private set; }
+
+        /// <summary>
+        /// Striche
+        /// </summary>
+        public static Einheit Striche { get; private set; }
+
+        /// <summary>
+        /// 1000 J/Sek.
+        /// </summary>
+        public static Einheit KilojouleProSekunde { get; private set; }
+
+        /// <summary>
+        /// Personen x Monate
+        /// </summary>
+        public static Einheit PersonenMalMonate { get; private set; }
+
+        /// <summary>
+        /// Personen
+        /// </summary>
+        public static Einheit Personen { get; private set; }
+
+        /// <summary>
+        /// Jahre
+        /// </summary>
+        public static Einheit Jahre { get; private set; }
+
+        /// <summary>
+        /// Monate
+        /// </summary>
+        public static Einheit Monate { get; private set; }
+
+        /// <summary>
+        /// Tage
+        /// </summary>
+        public static Einheit Tage { get; private set; }
+
+        /// <summary>
+        /// Gradtage
+        /// </summary>
+        public static Einheit Gradtage { get; private set; }
+
+        /// <summary>
+        /// Promille Anteile
+        /// </summary>
+        public static Einheit PromilleAnteile { get; private set; }
+
+        /// <summary>
+        /// Anzahl Rauchmelder
+        /// </summary>
+        public static Einheit AnzahlRauchwarnmelder { get; private set; }
+
+        /// <summary>
+        /// Prozent
+        /// </summary>
+        public static Einheit Prozent { get; private set; }
+
+        #endregion
+
+        private readonly string schluessel;
+        private readonly string bezeichnung;
+
+        /// <summary>
+        /// Liefert den Schlüssel der Einheit
+        /// </summary>
+        public string Schluessel { get { return schluessel; } }
+
+        /// <summary>
+        /// Liefert die Beschreibung der Einheit zu dem Schlüssel
+        /// </summary>
+        public string Bezeichnung { get { return bezeichnung; } }
+
+        private Einheit(string schluessel, string bezeichnung)
+        {
+            this.schluessel = schluessel;
+            this.bezeichnung = bezeichnung;
+        }
+
+        /// <summary>
+        /// Liefert die formatierte Ausgabe für die Verwendung in einer Datenaustauschdatei
+        /// </summary>
+        /// <returns>Der formatierte <see cref="string"/> für die Datenaustauschdatei</returns>
+        public override string ToString()
+        {
+            return schluessel;
+        }
+
+        /// <summary>
+        /// Ermittelt die <see cref="Einheit"/> aus der textuellen Repräsentation der Datenaustauschdatei 
+        /// </summary>
+        /// <param name="einheit">Der textuelle Wert aus dem Feld der Datenaustauschdatei</param>
+        /// <returns>Die ermittelte <see cref="Einheit"/></returns>
+        /// <exception cref="ArgumentNullException">Wenn als <paramref name="einheit"/> NULL übergeben wird.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Wenn als <paramref name="einheit"/> ein ungültiger Wert übergeben wird.</exception>
+        public static Einheit FromString(string einheit)
+        {
+            if (einheit == null)
+                throw new ArgumentNullException(nameof(einheit));
+            if (einheit.Length == 0 || !einheiten.ContainsKey(einheit))
+                throw new ArgumentOutOfRangeException(nameof(einheit));
+
+            return einheiten[einheit];
+        }
+    }
+}
---
+++ src/ArgeHeiwako.Tests/Data/Common/EinheitTests.cs
@@ -0,0 +1,261 @@
+﻿using ArgeHeiwako.Data.Common;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace ArgeHeiwako.Tests.Data.Common
+{
+    [ExcludeFromCodeCoverage]
+    public class EinheitTests
+    {
+        public EinheitTests()
+        {
+        }
+
+        #region Schnellzugriffe
+        [Fact]
+        public void GJ_GetsCorrectValue()
+        {
+            Assert.Equal("001", Einheit.GJ.Schluessel);
+            Assert.Equal("GJ", Einheit.GJ.Bezeichnung);
+        }
+
+        [Fact]
+        public void MWh_GetsCorrectValue()
+        {
+            Assert.Equal("002", Einheit.MWh.Schluessel);
+            Assert.Equal("MWh", Einheit.MWh.Bezeichnung);
+        }
+
+        [Fact]
+        public void Kubikmeter_GetsCorrectValue()
+        {
+            Assert.Equal("003", Einheit.Kubikmeter.Schluessel);
+            Assert.Equal("m³", Einheit.Kubikmeter.Bezeichnung);
+        }
+
+        [Fact]
+        public void Kalorien_GetsCorrectValue()
+        {
+            Assert.Equal("004", Einheit.Kalorien.Schluessel);
+            Assert.Equal("kcal", Einheit.Kalorien.Bezeichnung);
+        }
+
+        [Fact]
+        public void KilowattStunde_GetsCorrectValue()
+        {
+            Assert.Equal("005", Einheit.KilowattStunde.Schluessel);
+            Assert.Equal("kWh", Einheit.KilowattStunde.Bezeichnung);
+        }
+
+        [Fact]
+        public void WohnflaecheQm_GetsCorrectValue()
+        {
+            Assert.Equal("010", Einheit.QmWohnflaeche.Schluessel);
+            Assert.Equal("m² Wohnfläche", Einheit.QmWohnflaeche.Bezeichnung);
+        }
+
+        [Fact]
+        public void QmBeheizteWohnflaeche_GetsCorrectValue()
+        {
+            Assert.Equal("011", Einheit.QmBeheizteWohnflaeche.Schluessel);
+            Assert.Equal("m² beheizte Wohnfläche", Einheit.QmBeheizteWohnflaeche.Bezeichnung);
+        }
+
+        [Fact]
+        public void CmUmbauterRaum_GetsCorrectValue()
+        {
+            Assert.Equal("012", Einheit.CmUmbauterRaum.Schluessel);
+            Assert.Equal("m³ umbauter Raum", Einheit.CmUmbauterRaum.Bezeichnung);
+        }
+
+        [Fact]
+        public void CmBeheizterUmbauterRaum_GetsCorrectValue()
+        {
+            Assert.Equal("014", Einheit.CmBeheizterUmbauterRaum.Schluessel);
+            Assert.Equal("m³ beheizter umbauter Wohnraum", Einheit.CmBeheizterUmbauterRaum.Bezeichnung);
+        }
+
+        [Fact]
+        public void VariablesTextfeld_GetsCorrectValue()
+        {
+            Assert.Equal("015", Einheit.VariablesTextfeld.Schluessel);
+            Assert.Equal("variables Textfeld für Schlüssel", Einheit.VariablesTextfeld.Bezeichnung);
+        }
+
+        [Fact]
+        public void Miteigentumsanteil_GetsCorrectValue()
+        {
+            Assert.Equal("016", Einheit.Miteigentumsanteil.Schluessel);
+            Assert.Equal("Miteigentumsanteil", Einheit.Miteigentumsanteil.Bezeichnung);
+        }
+
+        [Fact]
+        public void QmNutzflaeche_GetsCorrectValue()
+        {
+            Assert.Equal("017", Einheit.QmNutzflaeche.Schluessel);
+            Assert.Equal("m² Nutzfläche", Einheit.QmNutzflaeche.Bezeichnung);
+        }
+
+        [Fact]
+        public void Anschlusswert_GetsCorrectValue()
+        {
+            Assert.Equal("020", Einheit.Anschlusswert.Schluessel);
+            Assert.Equal("Anschlusswert", Einheit.Anschlusswert.Bezeichnung);
+        }
+
+        [Fact]
+        public void Zaehler_GetsCorrectValue()
+        {
+            Assert.Equal("021", Einheit.Zaehler.Schluessel);
+            Assert.Equal("Zähler", Einheit.Zaehler.Bezeichnung);
+        }
+
+        [Fact]
+        public void Wohnung_GetsCorrectValue()
+        {
+            Assert.Equal("022", Einheit.Wohnung.Schluessel);
+            Assert.Equal("Wohnung", Einheit.Wohnung.Bezeichnung);
+        }
+
+        [Fact]
+        public void Abrechnung_GetsCorrectValue()
+        {
+            Assert.Equal("023", Einheit.Abrechnung.Schluessel);
+            Assert.Equal("Abrechnung", Einheit.Abrechnung.Bezeichnung);
+        }
+
+        [Fact]
+        public void Verbrauchseinheiten_GetsCorrectValue()
+        {
+            Assert.Equal("030", Einheit.Verbrauchseinheiten.Schluessel);
+            Assert.Equal("Verbrauchseinheiten (VE)", Einheit.Verbrauchseinheiten.Bezeichnung);
+        }
+
+        [Fact]
+        public void Verbrauchswerte_GetsCorrectValue()
+        {
+            Assert.Equal("031", Einheit.Verbrauchswerte.Schluessel);
+            Assert.Equal("Verbrauchswerte", Einheit.Verbrauchswerte.Bezeichnung);
+        }
+
+        [Fact]
+        public void Striche_GetsCorrectValue()
+        {
+            Assert.Equal("032", Einheit.Striche.Schluessel);
+            Assert.Equal("Striche (Venturi)", Einheit.Striche.Bezeichnung);
+        }
+
+        [Fact]
+        public void KilojouleProSekunde_GetsCorrectValue()
+        {
+            Assert.Equal("033", Einheit.KilojouleProSekunde.Schluessel);
+            Assert.Equal("1000 J/Sec.", Einheit.KilojouleProSekunde.Bezeichnung);
+        }
+
+        [Fact]
+        public void PersonenMalMonate_GetsCorrectValue()
+        {
+            Assert.Equal("034", Einheit.PersonenMalMonate.Schluessel);
+            Assert.Equal("Personen x Monate", Einheit.PersonenMalMonate.Bezeichnung);
+        }
+
+        [Fact]
+        public void Personen_GetsCorrectValue()
+        {
+            Assert.Equal("035", Einheit.Personen.Schluessel);
+            Assert.Equal("Personen", Einheit.Personen.Bezeichnung);
+        }
+
+        [Fact]
+        public void Prozent_GetsCorrectValue()
+        {
+            Assert.Equal("040", Einheit.Prozent.Schluessel);
+            Assert.Equal("Prozent", Einheit.Prozent.Bezeichnung);
+        }
+
+        [Fact]
+        public void Jahre_GetsCorrectValue()
+        {
+            Assert.Equal("041", Einheit.Jahre.Schluessel);
+            Assert.Equal("Jahr", Einheit.Jahre.Bezeichnung);
+        }
+
+        [Fact]
+        public void Monate_GetsCorrectValue()
+        {
+            Assert.Equal("042", Einheit.Monate.Schluessel);
+            Assert.Equal("Monat", Einheit.Monate.Bezeichnung);
+        }
+
+        [Fact]
+        public void Tage_GetsCorrectValue()
+        {
+            Assert.Equal("043", Einheit.Tage.Schluessel);
+            Assert.Equal("Tage", Einheit.Tage.Bezeichnung);
+        }
+
+        [Fact]
+        public void Gradtage_GetsCorrectValue()
+        {
+            Assert.Equal("044", Einheit.Gradtage.Schluessel);
+            Assert.Equal("Gradtage", Einheit.Gradtage.Bezeichnung);
+        }
+
+        [Fact]
+        public void PromilleAnteile_GetsCorrectValue()
+        {
+            Assert.Equal("045", Einheit.PromilleAnteile.Schluessel);
+            Assert.Equal("‰ – Anteile", Einheit.PromilleAnteile.Bezeichnung);
+        }
+
+        [Fact]
+        public void AnzahlRauchwarnmelder_GetsCorrectValue()
+        {
+            Assert.Equal("046", Einheit.AnzahlRauchwarnmelder.Schluessel);
+            Assert.Equal("Anzahl Rauchwarnmelder", Einheit.AnzahlRauchwarnmelder.Bezeichnung);
+        }
+
+        #endregion
+
+        #region FromString()
+
+        [Fact]
+        public void FromString_Null_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => Einheit.FromString(null));
+            Assert.Equal("einheit", ex.ParamName);
+        }
+
+        [Fact]
+        public void FromString_StringEmpty_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => Einheit.FromString(string.Empty));
+            Assert.Equal("einheit", ex.ParamName);
+        }
+
+        [Fact]
+        public void FromString_WithNumberNotKnown_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => Einheit.FromString("1"));
+            Assert.Equal("einheit", ex.ParamName);
+        }
+
+        [Fact]
+        public void FromString_WithNumberKnown_ReturnsCorrectInstance()
+        {
+            Assert.Equal(Einheit.GJ, Einheit.FromString("001"));
+        }
+
+        #endregion
+
+        #region ToString()
+
+        public void ToString_ReturnsCorrectValue()
+        {
+            Assert.Equal("001", Einheit.GJ.ToString());
+        }
+
+        #endregion
+    }
+}
