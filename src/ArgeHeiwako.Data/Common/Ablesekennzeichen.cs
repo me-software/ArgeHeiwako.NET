9f3073f9aa28d4db4393d9227c39e44a3826c28a
m4cx <macgyverx83@gmail.com>
2016-09-24 21:33:38 +0200

    Ablesekennzeichen (Tabelle S aus dem Datenaustausch) hinzugefügt

--- src/ArgeHeiwako.Data/Common/Ablesekennzeichen.cs
+++ src/ArgeHeiwako.Data/Common/Ablesekennzeichen.cs
@@ -1,12 +1,155 @@
-﻿namespace ArgeHeiwako.Data
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArgeHeiwako.Data.Common
 {
     /// <summary>
     /// Diese Klasse repräsentiert die für den Datenaustausch definierten Schätzungsaufteilungen sowie die 
     /// Ablesekennzeichen aus der Tabelle "S"
     /// </summary>
-    public class Ablesekennzeichen
+    public sealed class Ablesekennzeichen
     {
         #region Tabelle "S"
+
+        private static readonly IDictionary<string, Ablesekennzeichen> ablesekennzeichenDictionary = new Dictionary<string, Ablesekennzeichen> {
+             { "01", Schaetzung = new Ablesekennzeichen("01", "Schätzung") },
+             { "02", SchaetzungNachVorjahr = new Ablesekennzeichen("02", "Schätzung nach Vorjahr") },
+             { "03", SchaetzungNachNormwaermeleistung = new Ablesekennzeichen("03", "Schätzung nach Normwärmeleistung") },
+             { "04", SchaetzungNachGrundanteil = new Ablesekennzeichen("04", "Schätzung nach Grundanteil(z.B. 1 WMZ pro Nutzeinheit)") },
+             { "05", Teilschaetzung = new Ablesekennzeichen("05", "Teilschätzung") },
+             { "06", SchaetzungNachFlaeche = new Ablesekennzeichen("06", "Schätzung nach Fläche") },
+             { "07", SchaetzungNachVergleichbarenZeitraeumen = new Ablesekennzeichen("07", "Schätzung nach vergleichbaren Zeiträumen") },
+             { "08", SchaetzungNachDurchschnittsverbrauch = new Ablesekennzeichen("08", "Schätzung nach Durchschnittsverbrauch") },
+             { "10", NurKostenlieferung = new Ablesekennzeichen("10", "nur Kostenlieferung") },
+             { "11", Hauptablesung = new Ablesekennzeichen("11", "Hauptablesung") },
+             { "12", Zwischenablesung = new Ablesekennzeichen("12", "Zwischenablesung") },
+             { "13", AufteilungNachTagen = new Ablesekennzeichen("13", "Aufteilung nach Tagen") },
+             { "14", AufteilungNachGradtagen = new Ablesekennzeichen("14", "Aufteilung nach Gradtagen") },
+             { "15", SchaetzungNachVergleichbarenRaeumen = new Ablesekennzeichen("15", "Schätzung nach vergleichbaren Räumen") },
+            };
+        private readonly string schluessel;
+        private readonly string bezeichnung;
+
+        private Ablesekennzeichen(string schluessel, string bezeichnung)
+        {
+            this.schluessel = schluessel;
+            this.bezeichnung = bezeichnung;
+        }
+
         #endregion
+
+        #region Schnellzugriffe
+
+        /// <summary>
+        /// Schätzung
+        /// </summary>
+        public static Ablesekennzeichen Schaetzung { get; private set; }
+
+        /// <summary>
+        /// Schätzung nach Vorjahr
+        /// </summary>
+        public static Ablesekennzeichen SchaetzungNachVorjahr { get; private set; }
+
+        /// <summary>
+        /// Schätzung nach Normwärmeleistung
+        /// </summary>
+        public static Ablesekennzeichen SchaetzungNachNormwaermeleistung { get; private set; }
+
+        /// <summary>
+        /// Schätzung nach Grundanteil
+        /// </summary>
+        public static Ablesekennzeichen SchaetzungNachGrundanteil { get; private set; }
+
+        /// <summary>
+        /// Teilschätzung
+        /// </summary>
+        public static Ablesekennzeichen Teilschaetzung { get; private set; }
+
+        /// <summary>
+        /// Schätzung nach Fläche
+        /// </summary>
+        public static Ablesekennzeichen SchaetzungNachFlaeche { get; private set; }
+
+        /// <summary>
+        /// Schätzung nach vergleichbaren Zeiträumen
+        /// </summary>
+        public static Ablesekennzeichen SchaetzungNachVergleichbarenZeitraeumen { get; private set; }
+
+        /// <summary>
+        /// Schätzung nach Durchschnittsverbrauch
+        /// </summary>
+        public static Ablesekennzeichen SchaetzungNachDurchschnittsverbrauch { get; private set; }
+
+        /// <summary>
+        /// Nur Kostenlieferung
+        /// </summary>
+        public static Ablesekennzeichen NurKostenlieferung { get; private set; }
+
+        /// <summary>
+        /// Hauptablesung
+        /// </summary>
+        public static Ablesekennzeichen Hauptablesung { get; private set; }
+
+        /// <summary>
+        /// Zwischenablesung
+        /// </summary>
+        public static Ablesekennzeichen Zwischenablesung { get; private set; }
+
+        /// <summary>
+        /// Aufteilung nach Tagen
+        /// </summary>
+        public static Ablesekennzeichen AufteilungNachTagen { get; private set; }
+
+        /// <summary>
+        /// Aufteilung nach Gradtagen
+        /// </summary>
+        public static Ablesekennzeichen AufteilungNachGradtagen { get; private set; }
+
+        /// <summary>
+        /// Schätzung nach vergleichbaren Räumen
+        /// </summary>
+        public static Ablesekennzeichen SchaetzungNachVergleichbarenRaeumen { get; private set; }
+
+        #endregion
+
+        /// <summary>
+        /// Liefert den Schlüssel für das Ablesekennzeichen in der Datenaustauschdatei
+        /// </summary>
+        public string Schluessel { get { return schluessel; } }
+
+        /// <summary>
+        /// Liefert die textuelle Bedeutung des Schlüssels
+        /// </summary>
+        public string Bezeichnung { get { return bezeichnung; } }
+        
+        /// <summary>
+        /// Ermittelt das Ablesekennzeichen anhand des Schlüssels, der in der Datenaustauschdatei verwendet wird.
+        /// </summary>
+        /// <param name="ablesekennzeichen">Der textuelle Schlüssel aus der Datenaustauschdatei</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Wenn für <paramref name="ablesekennzeichen"/> NULL übergeben wird.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Wenn für den Wert in <paramref name="ablesekennzeichen"/> kein Ablesenkennzeichen bekannt ist.</exception>
+        public static Ablesekennzeichen FromString(string ablesekennzeichen)
+        {
+            if (ablesekennzeichen == null)
+            {
+                throw new ArgumentNullException(nameof(ablesekennzeichen));
+            }
+            if (ablesekennzeichen == string.Empty || !ablesekennzeichenDictionary.ContainsKey(ablesekennzeichen))
+            {
+                throw new ArgumentOutOfRangeException(nameof(ablesekennzeichen));
+            }
+
+            return ablesekennzeichenDictionary[ablesekennzeichen];
+        }
+
+        /// <summary>
+        /// Liefert die formatierte Ausgabe für die Verwendung in einer Datentausch-Datei
+        /// </summary>
+        /// <returns>Der formatierte String für die Datenaustauschdatei</returns>
+        public override string ToString()
+        {
+            return Schluessel;
+        }
     }
 }
---
+++ src/ArgeHeiwako.Tests/Data/Common/AblesekennzeichenTests.cs
@@ -0,0 +1,164 @@
+﻿using ArgeHeiwako.Data.Common;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace ArgeHeiwako.Tests.Data.Common
+{
+    [ExcludeFromCodeCoverage]
+    public sealed class AblesekennzeichenTests
+    {
+        public AblesekennzeichenTests()
+        {
+        }
+
+        #region Schnellzugriffe
+
+        [Fact]
+        public void Schaetzung_Get_ReturnsValue()
+        {
+            Assert.Equal("01", Ablesekennzeichen.Schaetzung.Schluessel);
+            Assert.Equal("Schätzung", Ablesekennzeichen.Schaetzung.Bezeichnung);
+        }
+
+        [Fact]
+        public void SchaetzungNachVorjahr_Get_ReturnsValue()
+        {
+            Assert.Equal("02", Ablesekennzeichen.SchaetzungNachVorjahr.Schluessel);
+            Assert.Equal("Schätzung nach Vorjahr", Ablesekennzeichen.SchaetzungNachVorjahr.Bezeichnung);
+        }
+
+        [Fact]
+        public void SchaetzungNachNormwaermeleistung_Get_ReturnsValue()
+        {
+            Assert.Equal("03", Ablesekennzeichen.SchaetzungNachNormwaermeleistung.Schluessel);
+            Assert.Equal("Schätzung nach Normwärmeleistung", Ablesekennzeichen.SchaetzungNachNormwaermeleistung.Bezeichnung);
+        }
+
+        [Fact]
+        public void SchaetzungNachGrundanteil_Get_ReturnsValue()
+        {
+            Assert.Equal("04", Ablesekennzeichen.SchaetzungNachGrundanteil.Schluessel);
+            Assert.Equal("Schätzung nach Grundanteil(z.B. 1 WMZ pro Nutzeinheit)", Ablesekennzeichen.SchaetzungNachGrundanteil.Bezeichnung);
+        }
+
+        [Fact]
+        public void Teilschaetzung_Get_ReturnsValue()
+        {
+            Assert.Equal("05", Ablesekennzeichen.Teilschaetzung.Schluessel);
+            Assert.Equal("Teilschätzung", Ablesekennzeichen.Teilschaetzung.Bezeichnung);
+        }
+
+        [Fact]
+        public void SchaetzungNachFlaeche_Get_ReturnsValue()
+        {
+            Assert.Equal("06", Ablesekennzeichen.SchaetzungNachFlaeche.Schluessel);
+            Assert.Equal("Schätzung nach Fläche", Ablesekennzeichen.SchaetzungNachFlaeche.Bezeichnung);
+        }
+
+        [Fact]
+        public void SchaetzungNachVergleichbarenZeitraeumen_Get_ReturnsValue()
+        {
+            Assert.Equal("07", Ablesekennzeichen.SchaetzungNachVergleichbarenZeitraeumen.Schluessel);
+            Assert.Equal("Schätzung nach vergleichbaren Zeiträumen", Ablesekennzeichen.SchaetzungNachVergleichbarenZeitraeumen.Bezeichnung);
+        }
+
+        [Fact]
+        public void SchaetzungNachDurchschnittsverbrauch_Get_ReturnsValue()
+        {
+            Assert.Equal("08", Ablesekennzeichen.SchaetzungNachDurchschnittsverbrauch.Schluessel);
+            Assert.Equal("Schätzung nach Durchschnittsverbrauch", Ablesekennzeichen.SchaetzungNachDurchschnittsverbrauch.Bezeichnung);
+        }
+
+        [Fact]
+        public void NurKostenlieferung_Get_ReturnsValue()
+        {
+            Assert.Equal("10", Ablesekennzeichen.NurKostenlieferung.Schluessel);
+            Assert.Equal("nur Kostenlieferung", Ablesekennzeichen.NurKostenlieferung.Bezeichnung);
+        }
+
+        [Fact]
+        public void Hauptablesung_Get_ReturnsValue()
+        {
+            Assert.Equal("11", Ablesekennzeichen.Hauptablesung.Schluessel);
+            Assert.Equal("Hauptablesung", Ablesekennzeichen.Hauptablesung.Bezeichnung);
+        }
+
+        [Fact]
+        public void Zwischenablesung_Get_ReturnsValue()
+        {
+            Assert.Equal("12", Ablesekennzeichen.Zwischenablesung.Schluessel);
+            Assert.Equal("Zwischenablesung", Ablesekennzeichen.Zwischenablesung.Bezeichnung);
+        }
+
+        [Fact]
+        public void AufteilungNachTagen_Get_ReturnsValue()
+        {
+            Assert.Equal("13", Ablesekennzeichen.AufteilungNachTagen.Schluessel);
+            Assert.Equal("Aufteilung nach Tagen", Ablesekennzeichen.AufteilungNachTagen.Bezeichnung);
+        }
+
+        [Fact]
+        public void AufteilungNachGradtagen_Get_ReturnsValue()
+        {
+            Assert.Equal("14", Ablesekennzeichen.AufteilungNachGradtagen.Schluessel);
+            Assert.Equal("Aufteilung nach Gradtagen", Ablesekennzeichen.AufteilungNachGradtagen.Bezeichnung);
+        }
+
+        [Fact]
+        public void SchaetzungNachVergleichbarenRaeumen_Get_ReturnsValue()
+        {
+            Assert.Equal("15", Ablesekennzeichen.SchaetzungNachVergleichbarenRaeumen.Schluessel);
+            Assert.Equal("Schätzung nach vergleichbaren Räumen", Ablesekennzeichen.SchaetzungNachVergleichbarenRaeumen.Bezeichnung);
+        }
+
+        #endregion
+
+        #region FromString()
+
+        [Fact]
+        public void FromString_Null_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => Ablesekennzeichen.FromString(null));
+            Assert.Equal("ablesekennzeichen", ex.ParamName);
+        }
+
+        [Fact]
+        public void FromString_StringEmpty_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => Ablesekennzeichen.FromString(string.Empty));
+            Assert.Equal("ablesekennzeichen", ex.ParamName);
+        }
+
+        [Fact]
+        public void FromString_StringNotKnown_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => Ablesekennzeichen.FromString("1"));
+            Assert.Equal("ablesekennzeichen", ex.ParamName);
+        }
+
+        [Fact]
+        public void FromString_StringKnown_ReturnsValue()
+        {
+            Assert.Equal(Ablesekennzeichen.Hauptablesung, Ablesekennzeichen.FromString(Ablesekennzeichen.Hauptablesung.Schluessel));
+        }
+
+        #endregion
+
+        #region ToString()
+
+        [Fact]
+        public void ToString_ReturnsStringLength2()
+        {
+            Assert.Equal(2, Ablesekennzeichen.Hauptablesung.ToString().Length);
+        }
+
+        [Fact]
+        public void ToString_ReturnsSchluesselValue()
+        {
+            Assert.Equal(Ablesekennzeichen.Hauptablesung.Schluessel, Ablesekennzeichen.Hauptablesung.ToString());
+        }
+
+        #endregion
+    }
+}
