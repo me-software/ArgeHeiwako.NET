a081c4e20fe4b389f0867897f64385b0370853b8
m4cx <macgyverx83@gmail.com>
2016-09-25 14:20:42 +0200

    Der Typ Anteil wurde implementiert #30

--- src/ArgeHeiwako.Data/Common/Anteil.cs
+++ src/ArgeHeiwako.Data/Common/Anteil.cs
@@ -1,4 +1,7 @@
-﻿namespace ArgeHeiwako.Data.Common
+﻿using System;
+using System.Globalization;
+
+namespace ArgeHeiwako.Data.Common
 {
     /// <summary>
     /// Numerische Angabe eines Anteils innerhalb des Datenaustausches 
@@ -6,5 +9,68 @@
     /// </summary>
     public class Anteil
     {
+        private readonly decimal anteil;
+
+        /// <summary>
+        /// Erstellt eine neue <see cref="Anteil"/>-Instanz
+        /// </summary>
+        /// <param name="anteil"></param>
+        public Anteil(decimal anteil)
+        {
+            this.anteil = CheckRange(anteil);
+        }
+
+        /// <summary>
+        /// Erstellt eine neue <see cref="Anteil"/>-Instanz
+        /// </summary>
+        /// <param name="anteil"></param>
+        public Anteil(string anteil)
+        {
+            if (anteil == null)
+                throw new ArgumentNullException(nameof(anteil));
+            if (anteil.Length != 9)
+                throw new ArgumentOutOfRangeException(nameof(anteil));
+
+            decimal value = 0m;
+            string anteilVal = anteil.Insert(6, ".");
+            if (!decimal.TryParse(anteilVal, NumberStyles.Any, CultureInfo.InvariantCulture, out value))
+                throw new ArgumentOutOfRangeException(nameof(anteil));
+
+            this.anteil = CheckRange(value);
+        }
+
+        /// <summary>
+        /// Liefert die formatierte Ausgabe für die Verwendung in einer Datentausch-Datei
+        /// </summary>
+        /// <returns>Der formatierte String für die Datenaustauschdatei</returns>
+        public override string ToString()
+        {
+            var isNegative = (anteil < 0);
+            var absBetrag = Math.Abs(anteil);
+            var truncated = Math.Truncate(absBetrag);
+            var fracture = (absBetrag - truncated) * 1000;
+
+            return isNegative ?
+                $"-{truncated:00000}{fracture:000}" :
+                $"{truncated:000000}{fracture:000}";
+        }
+
+        /// <summary>
+        /// Konvertiert eine <see cref="Anteil"/>-Instanz in seinen numerischen Wert.
+        /// </summary>
+        /// <param name="anteil">Die zu konvertierende <see cref="Anteil"/>-Instanz</param>
+        /// <returns>Den numerischen Wert der <see cref="Anteil"/>-Instanz</returns>
+        public static implicit operator decimal(Anteil anteil)
+        {
+            return anteil.anteil;
+        }
+
+        private decimal CheckRange(decimal anteil)
+        {
+            if (anteil < -99999.999M || anteil > 999999.999M)
+                throw new ArgumentOutOfRangeException(nameof(anteil));
+
+            return anteil;
+        }
     }
 }
---
+++ src/ArgeHeiwako.Tests/Data/Common/AnteilTests.cs
@@ -0,0 +1,116 @@
+﻿using ArgeHeiwako.Data.Common;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace ArgeHeiwako.Tests.Data.Common
+{
+    [ExcludeFromCodeCoverage]
+    public class AnteilTests
+    {
+        #region Ctor
+
+        [Fact]
+        public void Ctor_Null_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new Anteil(null));
+            Assert.Equal("anteil", ex.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_EmptyString_ThrowsArgumentOutOfRangeException()
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Anteil(string.Empty));
+            Assert.Equal("anteil", ex.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_StringLength9_ThrowsArgumentOutOfRangeException()
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Anteil(new string('0', 8)));
+            Assert.Equal("anteil", ex.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_StringLength11_ThrowsArgumentOutOfRangeException()
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Anteil(new string('0', 10)));
+            Assert.Equal("anteil", ex.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_StringNotNumericCorrectLength_ThrowsArgumentOutOfRangeException()
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Anteil(new string('A', 9)));
+            Assert.Equal("anteil", ex.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_StringNumeric000000000_CreatesInstance()
+        {
+            Assert.NotNull(new Anteil(new string('0', 9)));
+        }
+
+        [Fact]
+        public void Ctor_Number10000000Dot01_ThrowsArgumentOutOfRangeException()
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Anteil(1000000.001M));
+            Assert.Equal("anteil", ex.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_Number0_ThrowsArgumentOutOfRangeException()
+        {
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Anteil(-100000.000M));
+            Assert.Equal("anteil", ex.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_Number0_CreatesInstance()
+        {
+            Assert.NotNull(new Anteil(0));
+        }
+
+        #endregion
+
+        #region ToString()
+
+        [Theory]
+        [InlineData("-00000001", -.001)]
+        [InlineData("-00001000", -1)]
+        [InlineData("000000001", .001)]
+        [InlineData("000000010", .01)]
+        [InlineData("000000100", .1)]
+        [InlineData("000000000", 0)]
+        [InlineData("000001000", 1)]
+        [InlineData("000010000", 10)]
+        [InlineData("000100000", 100)]
+        [InlineData("001000000", 1000)]
+        [InlineData("010000000", 10000)]
+        [InlineData("100000000", 100000)]
+        public void ToString_Data_ReturnsExpectedStringValue(string expectedValue, decimal data)
+        {
+            Assert.Equal(expectedValue, new Anteil(data).ToString());
+        }
+
+        #endregion
+
+        #region op_Implicit_Decimal
+
+        [Theory]
+        [InlineData(1, "000001000")]
+        [InlineData(.1, "000000100")]
+        [InlineData(.001, "000000001")]
+        [InlineData(10, "000010000")]
+        [InlineData(100, "000100000")]
+        [InlineData(1000, "001000000")]
+        [InlineData(10000, "010000000")]
+        [InlineData(100000, "100000000")]
+        public void OpImplicit_StringValue_ReturnsExpectedValue(decimal expectedValue, string data)
+        {
+            Assert.Equal(expectedValue, new Anteil(data));
+        }
+
+        #endregion
+    }
+}
